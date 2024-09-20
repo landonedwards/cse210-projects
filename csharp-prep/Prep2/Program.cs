1653c6557fb49bcd8e69d0f913a4892b82938015
landonedwards <lando.s.edwards@gmail.com>
2024-09-20 15:53:14 -0600

    Completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string answer = Console.ReadLine();
+        int gradePercentage = int.Parse(answer);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+            {
+                letter = "A";
+            }
+        
+        else if (gradePercentage >= 80)
+            {
+                letter = "B";
+            }
+       
+        else if (gradePercentage >= 70)
+            {
+                letter = "C";
+            }
+        
+        else if (gradePercentage >= 60)
+            {
+                letter = "D";
+            }
+        
+        else
+            {
+                letter = "F";
+            }
+        
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class.");
+        }
+
+        else
+        {
+            Console.WriteLine("You failed. Don't worry! There's always next time.");
+        }
+        
     }
 }
