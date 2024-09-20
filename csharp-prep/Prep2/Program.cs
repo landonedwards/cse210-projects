28479f23368892e89898d41986a4e1fa22f55aae
landonedwards <lando.s.edwards@gmail.com>
2024-09-20 15:55:12 -0600

    Corrected formatting on prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -11,30 +11,30 @@ class Program
         string letter = "";
 
         if (gradePercentage >= 90)
-            {
-                letter = "A";
-            }
-        
+        {
+            letter = "A";
+        }
+
         else if (gradePercentage >= 80)
-            {
-                letter = "B";
-            }
-       
+        {
+            letter = "B";
+        }
+
         else if (gradePercentage >= 70)
-            {
-                letter = "C";
-            }
-        
+        {
+            letter = "C";
+        }
+
         else if (gradePercentage >= 60)
-            {
-                letter = "D";
-            }
-        
+        {
+            letter = "D";
+        }
+
         else
-            {
-                letter = "F";
-            }
-        
+        {
+            letter = "F";
+        }
+
         Console.WriteLine($"Your grade is: {letter}");
 
         if (gradePercentage >= 70)
@@ -46,6 +46,6 @@ class Program
         {
             Console.WriteLine("You failed. Don't worry! There's always next time.");
         }
-        
+
     }
 }
