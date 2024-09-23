d7439f34cc5d3a270c953949b1989e0344ddb0ab
landonedwards <lando.s.edwards@gmail.com>
2024-09-23 13:40:42 -0600

    created the one to ten program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+
+        if (color == "purple" || color == "Purple")
+        {
+            Console.WriteLine("Purple is my favorite color too.");
+        }
+
+        else
+        {
+            Console.WriteLine($"{color} is not my favorite.");
+        }
+
     }
 }
