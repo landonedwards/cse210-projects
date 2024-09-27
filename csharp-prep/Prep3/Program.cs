1dd22cd937e9b12720adf809de958aa62e45f295
landonedwards <lando.s.edwards@gmail.com>
2024-09-27 15:47:42 -0600

    completed c prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        // Console.Write("What is the magic number? ");
+        // string mNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(mNumber);
+
+        int userGuess = -1;
+
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string mGuess = Console.ReadLine();
+            userGuess = int.Parse(mGuess);
+
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Try lower");
+            }
+
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Try higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
