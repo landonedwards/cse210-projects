e02d1972d79f7d0945030b966eeb186c9579651f
landonedwards <lando.s.edwards@gmail.com>
2024-09-27 15:07:35 -0600

    added functionailty to sandbox2 and 3

--- sandbox/Sandbox2/Program.cs
+++ sandbox/Sandbox2/Program.cs
@@ -4,10 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<double> x;
-        x = [24, 25, 26.27];
+        List<double> x = new();
+        // x.Add(24.5);
 
         Console.WriteLine("Howdy, Potato World!");
+
+        // gather data to fill up a list
+
+        double input = -1;
+        while(input != 0)
+        {
+            Console.Write("Enter something: ");
+            string input_string = Console.ReadLine();
+            input = double.Parse(input_string);
+            x.Add(input);
+        }
 
         foreach (double number in x)
         {
--- sandbox/Sandbox3/Program.cs
+++ sandbox/Sandbox3/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static void AwesomeFunction(int x)
+{
+    Console.WriteLine($"The argument received by the awesome function is {x}.");
+}
+
+AwesomeFunction(189999);
