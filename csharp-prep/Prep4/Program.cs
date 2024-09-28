8cb6bd268582674926755eb043d5350b87d4ea37
landonedwards <lando.s.edwards@gmail.com>
2024-09-27 18:36:44 -0600

    completed c prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -999999;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string sNumber = Console.ReadLine();
+            userNumber = int.Parse(sNumber);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+        } while (userNumber != 0);
+
+        int sum = 0;
+        int largest = -999999;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        float average = ((float)sum) / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
